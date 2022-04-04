99a2aa8b6aca69d73b5b1017bdc72b215d862a51
PriyaRam <priyahanu2228@gmail.com>
2022-04-04 23:35:33 +0530

    Custom module creation

---
+++ web/modules/custom/gotjunk_us/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.gotjunk_us = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
