6a4c436564bdd47bbcec970fcc88bd5760101d21
Nicholas Bernandez <nicholas.b@listertechnologies.com>
2022-04-05 15:23:08 +0530

    footer social menu and some style changes

--- web/themes/custom/gotjunk_us/js/global.js
+++ web/themes/custom/gotjunk_us/js/global.js
@@ -9,7 +9,14 @@
 
   Drupal.behaviors.gotjunk_us = {
     attach: function (context, settings) {
-
+      jQuery('#block-submenu').hide();
+      jQuery('.jobs-more-menu').click(
+        function(){
+          jQuery('#block-submenu').toggle();
+        }
+      );
+      
+    jQuery(".find-a-job a").replaceWith('<a class="use-ajax" data-dialog-options="{&quot;width&quot;:400}" data-dialog-type="modal" href="/find-jobs-on-indeed">Find A Job</a>');	
     }
   };
 
