1123c38156076128f1023f436aa51271ab64320b
Nicholas Bernandez <nicholas.b@listertechnologies.com>
2022-04-07 20:42:47 +0530

    Home page design changes

--- web/themes/custom/gotjunk_us/js/global.js
+++ web/themes/custom/gotjunk_us/js/global.js
@@ -15,7 +15,16 @@
           jQuery('#block-submenu').toggle();
         }
       );
-      
+      jQuery('.read-more-expand').click(
+        function(){
+          jQuery('#read-more').slideDown();
+        }
+      );
+      jQuery('.read-more-collapse').click(
+        function(){
+          jQuery('#read-more').slideUp();
+        }
+      );
     jQuery(".find-a-job a").replaceWith('<a class="use-ajax" data-dialog-options="{&quot;width&quot;:400}" data-dialog-type="modal" href="/find-jobs-on-indeed">Find A Job</a>');	
     }
   };
