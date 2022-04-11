646a441c3c414dd2aefac410e835e510a2a9ab6a
Nicholas Bernandez <nicholas.b@listertechnologies.com>
2022-04-11 14:23:50 +0530

    globaljs fix

--- web/themes/custom/gotjunk_us/js/global.js
+++ web/themes/custom/gotjunk_us/js/global.js
@@ -24,7 +24,7 @@
       $('.read-more-collapse').click(
         function(){
           $('#read-more').slideUp();
-          $('read-more-expand').show();
+          $('.read-more-expand').show();
         }
       );
       $(".find-a-job a").replaceWith('<a class="use-ajax" data-dialog-options="{&quot;width&quot;:400}" data-dialog-type="modal" href="/find-jobs-on-indeed">Find A Job</a>');	
